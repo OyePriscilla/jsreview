6d05d39b2fdffe6411361dcc9f23258ed7dc86ac
Oyebade Priscilla <103386183+OyePriscilla@users.noreply.github.com>
2022-07-11 18:31:35 +0100

    Remove space

--- src/index.js
+++ src/index.js
@@ -29,14 +29,14 @@ const displayTodo = () => {
     todos.forEach((todo, id) => {
       li += `
       <div class = todo-dynamic>
-      <label for id = "${id}" class="todo-input">
-          <input onclick = 'update(this)' class = 'check-task' type = "checkbox" id = "${id}">
-          <p class = "todo-text">${todo.description}</p>
+      <label for id="${id}" class="todo-input">
+          <input onclick='update(this)' class='check-task' type="checkbox" id="${id}">
+          <p class="todo-text">${todo.description}</p>
       </label>
-      <div class= "settings">
-         <i class = "fa fa-ellipsis-v"></i>
-        <div class = "delete">
-        <i onclick = "deleteTask(${id})" class = "fa fa-trash"></i>
+      <div class="settings">
+         <i class="fa fa-ellipsis-v"></i>
+        <div class="delete">
+        <i onclick="deleteTask(${id})" class="fa fa-trash"></i>
         </div>
       </div>
   </div>
